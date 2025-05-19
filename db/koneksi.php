10f8b8f6099cbe938ec7ca33ea47e65b52a9e932
nissa8888 <nisaawildani@gmail.com>
2025-05-20 00:10:11 +0700

    koneksi.php

--- db/koneksi.php
+++ db/koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$name = "toko";
+$name = "Frontend_layanan_web";
 
 $koneksi = mysqli_connect($host,  $user,  $pass) or die("Koneksi ke database gagal!");
 mysqli_select_db($koneksi, $name) or die("Tidak ada database yang dipilih!");
