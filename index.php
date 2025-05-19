5880bb129fa0e3d2b00306e9521449c45ef636d9
nissa8888 <nisaawildani@gmail.com>
2025-05-19 15:41:42 +0700

    set up Login

---
+++ index.php
@@ -0,0 +1,262 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <!---- font awesom --->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!------fonts--->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet">
+    <!-- css style ----->
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <!----animation----->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!------favicon------>
+    <link rel="shortcut icon" href="">
+    <!---------------------------------------->
+    <title>Aerostreet</title>
+  </head>
+
+  <body>
+    <!------ navbar------->
+    <section class="nav-navbar fixed-top  bg-light">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+          <a class="navbar-brand" href="index.html">Aerostreet</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto">
+              <a class="nav-item nav-link" href="#home">Halaman <span class="sr-only">(current)</span></a>
+              <a class="nav-item nav-link" href="#about">Tentang Kami</a>
+              <a class="nav-item nav-link" href="#produk">Produk</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </section>
+    <!-------------endl navbar-------->
+    <!------------crousel----------->
+    <section id="home" class="awal">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block w-100" src="./assets/img/1.png" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="./assets/img/2.png" alt="Second slide">
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="card">
+                  <div class="row">
+                    <div class="col-md-8 col-9">
+                      <div class="card-body">
+                        Pria
+                      </div>
+                    </div>
+                    <div class="col-md-4 col-3">
+                      <img src="./assets/img/sepatu/2.png" class="img-fluid" alt="Responsive image">
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="card">
+                  <div class="row">
+                    <div class="col-md-8 col-9">
+                      <div class="card-body">
+                        Wanita
+                      </div>
+                    </div>
+                    <div class="col-md-4 col-3">
+                      <img src="./assets/img/sepatu/3.png" class="img-fluid" alt="Responsive image">
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="card">
+                  <div class="row">
+                    <div class="col-md-8 col-9">
+                      <div class="card-body">
+                        Unisex
+                      </div>
+                    </div>
+                    <div class="col-md-4 col-3">
+                      <img src="./assets/img/sepatu/4.png" class="img-fluid" alt="Responsive image">
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="card">
+                  <div class="row">
+                    <div class="col-md-8 col-9">
+                      <div class="card-body">
+                        Anak
+                      </div>
+                    </div>
+                    <div class="col-md-4 col-3">
+                      <img src="./assets/img/sepatu/1.png" class="img-fluid" alt="Responsive image">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--------------about----------->
+    <section id="about" class="about bg-white mt-5">
+      <div class="container">
+        <div class="row mb-4">
+          <div class="col-lg-9 sec-2 mt-5">
+            <h2>Tentang Kami</h2>
+            <p>Bisnis yang dimulai oleh Adhitya ini dimulai sejak tahun 2015, di mana saat itu ia melihat bahwa ada
+              kebutuhan sekunder berupa sepatu yang sekiranya dapat dijangkau oleh seluruh lapisan masyarakat. Seperti
+              slogan dari Aerostreet “Now everyone can buy a good shoes,” itulah yang menjadi harapan Adhitya bahwa
+              semua masyarakat Indonesia dari kalangan manapun, dapat membeli sepatu berkualitas dengan harga yang
+              terjangkau. <br></p>
+          </div>
+          <div class="col-lg-3">
+            <img src="assets/icon/Logo_Aerostreet.png" alt="logo" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+    <!------------- product ------------------->
+    <section class="brand mt-5 bg-custom">
+      <div class="container">
+        <div class="row text-center pb-5 pt-2">
+          <div class="col-6 col-lg-3 mt-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="./assets/icon/Logo/1.png" width="80px" class="img-fluid" alt="Responsive image">
+              </div>
+            </div>
+          </div>
+          <div class="col-6 col-lg-3 mt-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="./assets/icon/Logo/2.png" width="80px" class="img-fluid" alt="Responsive image">
+              </div>
+            </div>
+          </div>
+          <div class="col-6 col-lg-3 mt-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="./assets/icon/Logo/3.png" width="80px" class="img-fluid" alt="Responsive image">
+              </div>
+            </div>
+          </div>
+          <div class="col-6 col-lg-3 mt-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="./assets/icon/Logo/4.png" width="80px" class="img-fluid" alt="Responsive image">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!------------- product ------------------->
+    <section class="quote bg-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h1>Now everyone can buy a good shoes</h1>
+            <p>-Aerostreet.co.id</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!------------- product ------------------->
+    <section id="produk" class="produk mt-5 mb-5">
+      <div class="container">
+        <div class="row">
+          <h2>Our Product</h2>
+        </div>
+        <div class="row text-center">
+          <?php
+          require './db/koneksi.php';
+          $sql = mysqli_query($koneksi, "SELECT * FROM produk");
+          while ($data = mysqli_fetch_assoc($sql)) {
+            ?>
+          <div class="col-md-6 col-lg-3 mt-3">
+            <div class="card">
+              <img class="card-img-top" src="./assets/produk/<?php echo $data['gambar']; ?>"
+                alt="<?php echo $data['gambar']; ?>">
+              <div class="card-body">
+                <h4 class="card-title "><?php echo $data['nama']; ?></h4>
+                <h6 class="pb-3"><?php echo $data['harga']; ?></h6>
+                <button class="btn btn-primary btn-block btn-custom" data-toggle="modal"
+                  data-target="#pesan<?php echo $data['id']; ?>"><i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                  Beli</button>
+              </div>
+            </div>
+          </div>
+          <?php
+          include 'proses.php';
+          ini_set("display_errors", "Off");
+          }
+          ?>
+        </div>
+      </div>
+    </section>
+    <!------------- footer ------------------->
+    <div class="footer bg-custom">
+      <div class="card-footer text-white text-center ">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 pt-3 pb-3">
+              <span>Copyright &copy; <a>KELOMPOK 3 UNPAM - </a> 2024</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!------------- end footer ------------------->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+      crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/costum.js"></script>
+  </body>
+
+</html>
